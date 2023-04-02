7d821c12de29ed31c227a708cc79068476be4694
Yuki <yutaroud@yahoo.co.jp>
2023-04-02 23:01:19 +0900

    showの作成

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ func main() {
     router := gin.Default()
     router.GET("/albums", getAlbums)
     router.POST("/albums", postAlbums)
+    router.GET("/albums/:id", getAlbumByID)
 
     router.Run("localhost:8080")
 }
@@ -44,4 +45,18 @@ func postAlbums(c *gin.Context) {
     // Add the new album to the slice.
     albums = append(albums, newAlbum)
     c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumByID(c *gin.Context) {
+    id := c.Param("id")
+
+    // Loop over the list of albums, looking for
+    // an album whose ID value matches the parameter.
+    for _, a := range albums {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
